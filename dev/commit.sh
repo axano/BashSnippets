93ed0646882d0e99ae05819a9917cea73f845bc9
Emmanouil Perselis <okmanolos1@hotmail.com>
2018-09-10 12:59:09 +0200

    Create commit.sh

---
+++ dev/commit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+###############################
+###
+###   FIRST ARGUMENT IS THE COMMIT MESSAGE
+###   IF THERE IS NO COMMIT MESSAGE A STANDARD MESSAGE WILL BE USED
+###   THIS SCRIPT RUNS getGitLines.sh first to update the total lines count
+###
+###############################
+standardCommitMessage="Commited with script."
+./getLines.sh
+git add .
+if [[ $# -le 0 ]]; then
+  git commit -m "$standardCommitMessage"
+else
+  git commit -m "$1"
+fi
+
+git push
